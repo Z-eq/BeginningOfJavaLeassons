e24b840167f9bcaaa74760d0916c55c3f43e64e3
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-07 22:37:46 +0200

    Arrays in Methods lesson 32

--- ArraysInMethodsLesson32.java
+++ ArraysInMethodsLesson32.java
@@ -7,13 +7,14 @@ public class ArraysInMethodsLesson32 {
 
         int zeq[] = {2, 3, 4, 5, 6};
         change(zeq);
-        for(int y:zeq)
+        for (int y : zeq)
             System.out.println(y);
 
     }
+
     public static void change(int x[]) {  // in this part the important thing is we added an array [] instead of just an variable!
-        for (int counter=0;counter<x.length;counter++)
-            x[counter]+=5;  // this one add 5 to indexes of arrays
+        for (int counter = 0; counter < x.length; counter++)
+            x[counter] += 5;  // this one add 5 to indexes of arrays
 
     }
 
